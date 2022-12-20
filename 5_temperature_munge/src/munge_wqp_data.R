53f6b1d769738abcf5c24c1a9884579383bf2968
Simon Topp <33098707+SimonTopp@users.noreply.github.com>
2022-12-20 08:34:50 -0500

    updated date filter to sys.date

--- 5_temperature_munge/src/munge_wqp_data.R
+++ 5_temperature_munge/src/munge_wqp_data.R
@@ -113,8 +113,8 @@ combine_temp_sources <- function(outind, wqp_daily_ind, superset_daily_ind, cell
   bind_rows(wqp_daily, new_data) %>% arrange(site_id) %>%
     filter(!site_id %in% remove_ids) %>%
     filter(site_id %in% cell_sites$site_id) %>%
-    filter(Date < as.Date('2021-01-01'), Date > as.Date('1979-12-31')) %>% 
-    group_by(Date, site_id) %>% 
+    filter(Date < as.Date(Sys.Date()), Date > as.Date('1979-12-31')) %>%
+  	group_by(Date, site_id) %>% 
     summarise(wtemp = first(wtemp), source = first(source), 
               .groups = 'drop') %>% 
     write_feather(path = outfile)
